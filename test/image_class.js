bb495e9b24de631e1c14b4b2c548d8ad47634f59
ivancho277 <rouskoi@live.com>
2019-04-14 15:25:51 -0700

    fun

---
+++ test/image_class.js
@@ -0,0 +1,14 @@
+class Image {
+    constructor(id, id_cloudinary, url, userId, tag, locationName, description, isPublic ){
+        this.id
+        this.id_cloudinary
+        this.url
+        this.user_id
+        this.tag
+        this.location_name
+        this.description
+        this.public
+    }
+}
+
+module.exports = Image;
