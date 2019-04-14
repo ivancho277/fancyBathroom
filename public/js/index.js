958c8e40f57152be8ed91964bf091a5f6b3c711b
Monte Cristo <mjblee20@Bills-MacBook-Pro.local>
2019-04-14 13:47:41 -0700

    handlebars WIP

--- public/js/index.js
+++
@@ -1,7 +0,0 @@
-// load login modal on button click 
-$("#login-modal").modal("show");
-$("#modal-button").on("click", function () {
-    $("#login-modal").modal("show");
-});
-
-res.render('index', {favorites: true});
