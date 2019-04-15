ee4533d2f11e26484e4bedc6cca65e492a53ef3b
kth <katiehaster@gmail.com>
2019-04-15 14:27:00 -0700

    map in progress, testing. not for deployment

--- public/js/public_index.js
+++ public/js/public_index.js
@@ -1,11 +1,12 @@
+var location_name;
+let seattle =  {lat: 47.608013, lng: -122.335167};
+let placesArray = [];
 
 $("#fun").on("click", function() {
     console.log("button pressed");
     $.post("/api/users", {userid:"hhww", userName: "sailormoon"}, function(err, result) {
-        console.log("in ajax post call", result);
-       
+        console.log("in ajax post call", result);  
     });
-
 });
 // $.get("/api/users", function(err, result) {
 //     console.log(result);
@@ -26,8 +27,59 @@ function initMap() {
 
 // Event listener - grabs name of location and address from user input
 $("#submit").click(function () {
-    var location_name = {
-       name: $("input").val().trim(),
-    }
+    location_name = $("input").val().trim()
+    
     console.log("location name: ", location_name);
+});
+
+// Event listener - on click of location button, sends user to google map w/ icon
+$(".location").click(function () {
+    console.log("hello!")
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 47.608013, lng: -122.335167},
+        zoom: 10
+      });
+
+      function queryPlace() {
+        let request = {
+            location: seattle,
+            radius: 5000,
+            keyword: location_name,
+            fields: ['name', 'geometry']
+        };
+        service = new google.maps.places.PlacesService(map);
+        service.nearbySearch(request, (results, status) => {
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
+                    results[0].geometry.lat //assign these to a global var
+                    results[0].geometry.lng;
+                    
+            }
+            //use this lat and lng to create a marker and then drop it
+            placeDetailsFromSearch(parkMarkArray);
+        })
+
+    //   var marker = new google.maps.Marker({
+    //     position: will want to put lat and long in here,
+    //     map: map,
+    //   });
+
+      if (place.geometry.viewport) {
+        map.fitBounds(place.geometry.viewport);
+      } else {
+        map.setCenter(place.geometry.location);
+        map.setZoom(17);  // Why 17? Because it looks good.
+      }
+      marker.setPosition(place.geometry.location);
+      marker.setVisible(true);
+     
+
+    //   var address = '';
+    //   if (place.address_components) {
+    //     address = [
+    //       (place.address_components[0] && place.address_components[0].short_name || ''),
+    //       (place.address_components[1] && place.address_components[1].short_name || ''),
+    //       (place.address_components[2] && place.address_components[2].short_name || '')
+    //     ].join(' ');
+    //   }
+    }
 });
