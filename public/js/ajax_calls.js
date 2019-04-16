6ef0faa574b01bb03ecf788a822ec99aecee7d42
cplank <cplankshepard@gmail.com>
2019-04-15 20:15:32 -0700

    got cloudinary widget working with server

--- public/js/ajax_calls.js
+++ public/js/ajax_calls.js
@@ -1,6 +1,6 @@
 //TESTING OBJECTS
 const User = require("../test/user_class");
-const Image = require("../test/image_class");
+const Picture = require("../test/image_class");
 
 let user1 = new User("qq01pp", "sailorMoon")
 let user2 = new User("ww02oo", "sailorMercury")
@@ -8,11 +8,11 @@ let user3 = new User("ee03ii", "sailorVenus")
 let user4 = new User("rr04uu", "sailorMars")
 let user5 = new User("tt05yy", "sailorJupiter")
 
-let image1 = new Image("www.funPIC.super", "qq01pp", "luxury", "the mall", "a cool mall bathroom", true)
-let image2 = new Image("Cloud-444-id", "www.funPIC.duper", "ww02oo", "luxury", "the mall", "a cool mall bathroom", true)
-let image3 = new Image("luxury", "the mall", "a cool mall bathroom", true)
-let image4 = new Image("Cloud-143-id", "www.funPIC.heynow", "rr04uu", "luxury", "the mall", "a cool mall bathroom", true)
-let image5 = new Image("Cloud-555-id", "www.funPIC.yolo", "tt05yy", "luxury", "the mall", "a cool mall bathroom", true)
+let image1 = new Picture("www.funPIC.super", "qq01pp", "luxury", "the mall", "a cool mall bathroom", true)
+let image2 = new Picture("Cloud-444-id", "www.funPIC.duper", "ww02oo", "luxury", "the mall", "a cool mall bathroom", true)
+let image3 = new Picture("luxury", "the mall", "a cool mall bathroom", true)
+let image4 = new Picture("Cloud-143-id", "www.funPIC.heynow", "rr04uu", "luxury", "the mall", "a cool mall bathroom", true)
+let image5 = new Picture("Cloud-555-id", "www.funPIC.yolo", "tt05yy", "luxury", "the mall", "a cool mall bathroom", true)
 
 // CRUD Operations
 // Find One Sequelize Method
--- public/js/interactions.js
+++ public/js/interactions.js
@@ -17,7 +17,7 @@ class Picture {
 //Onclick for cloudinary upload
 let submitAllow = false;
 let imageInfo = {};
-let imageObject = {};
+let imageObj = {};
 
 var widget = cloudinary.createUploadWidget({
     cloudName: "instapotty", uploadPreset: "wveqgdsr"
@@ -28,12 +28,11 @@ var widget = cloudinary.createUploadWidget({
         if (result.event === "success") {
             submitAllow = true;
             console.log("allow", submitAllow);
-            let data = result.event.info;
 
             // save imageInfo into object
-            imageInfo.cloudinary = data.public_id;
-            imageInfo.thumbnailUrl = data.thumbnail_url;
-            imageInfo.url = data.url;
+            imageInfo.cloudinary = result.info.public_id;
+            imageInfo.thumbnailUrl = result.info.thumbnail_url;
+            imageInfo.url = result.info.url;
 
             // Change text of button from Upload Image to Change Image
             $("#upload_widget").text("Change Image");
@@ -46,41 +45,41 @@ document.getElementById("upload_widget").addEventListener("click", function () {
     widget.open();
 }, false);
 
-$("#submit").on("click", function () {
-    if (submitAllow) {
-        var public;
-        // cloud-id, url, user-id, tag, location-name, description, public
+// $("#submit").on("click", function () {
+//     if (submitAllow) {
+//         var public;
+//         // cloud-id, url, user-id, tag, location-name, description, public
 
-        // not sure if will work
-        if ($("input[name='public']:checked")) {
-            public = true;
-        } else {
-            public = false;
-        }
-        var imageObj = new Picture(imageInfo.cloudinary, imageInfo.url, "user-id", $("#category").val(), $("#location").val().trim(), $("#description").val().trim(), public);
-        console.log("obj added to database", imageObj);
-    }
-    submitAllow = false;
-});
+//         // not sure if will work
+//         if ($("input[name='public']:checked")) {
+//             public = true;
+//         } else {
+//             public = false;
+//         }
+//         var imageObj = new Picture(imageInfo.cloudinary, imageInfo.url, "user-id", $("#category").val(), $("#location").val().trim(), $("#description").val().trim(), public);
+//         console.log("obj added to database", imageObj);
+//     }
+//     submitAllow = false;
+// });
 
 
 // Connects to google maps API
 // Search location field - autocomplete
-function initMap() {
-    var input = document.getElementById("userInput");
+// function initMap() {
+//     var input = document.getElementById("userInput");
 
-    var autocomplete = new google.maps.places.Autocomplete(input);
-    console.log("autocomplete: ", autocomplete);
+//     var autocomplete = new google.maps.places.Autocomplete(input);
+//     console.log("autocomplete: ", autocomplete);
 
-    // Set the data fields to return when the user selects a place.
-    autocomplete.setFields(
-        ["address_components", "geometry", "icon", "name"]);
-}
+//     // Set the data fields to return when the user selects a place.
+//     autocomplete.setFields(
+//         ["address_components", "geometry", "icon", "name"]);
+// }
 
 // Event listener - grabs name of location and address from user input
-$("#submit").click(function () {
-    var location_name = {
-        name: $("input").val().trim(),
-    }
-    console.log("location name: ", location_name);
-});
+// $("#submit").click(function () {
+//     var location_name = {
+//         name: $("input").val().trim(),
+//     }
+//     console.log("location name: ", location_name);
+// });
