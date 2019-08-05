a027365a4e540a286d79f2047b87df1d1adf69ba
cplank <cplankshepard@gmail.com>
2019-08-05 12:34:37 -0700

    setting up connection for deployment

--- server.js
+++ server.js
@@ -1,32 +1,55 @@
-// Requiring necessary npm packages
+// SERVER TAKE TWO 
+const express = require("express");
+const exphbs = require("express-handlebars");
 
-var express = require("express");
+const app = express();
 
-// Setting up port and requiring models for syncing
-var PORT = process.env.PORT || 3000;
-var db = require("./models");
+const PORT = process.env.PORT || 8008;
 
-// Creating express app and configuring middleware needed for authentication
-var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static("public"));
-// We need to use sessions to keep track of our user's login status
-//app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
-
-
-// Set Handlebars.
-var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Requiring our routes
 require("./routes/api-routes.js")(app);
 
-// Syncing our database and logging a message to the user upon success
-db.sequelize.sync().then(function () {
-  app.listen(PORT, function () {
-    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
-  });
+app.listen(PORT, function () {
+  console.log("App now listening at localhost:" + PORT)
 });
+
+
+// // Requiring necessary npm packages
+
+// var express = require("express");
+
+// // Setting up port and requiring models for syncing
+// var PORT = process.env.PORT || 3000;
+// var db = require("./models");
+
+// // Creating express app and configuring middleware needed for authentication
+// var app = express();
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.static("public"));
+// // We need to use sessions to keep track of our user's login status
+// //app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+
+
+// // Set Handlebars.
+// var exphbs = require("express-handlebars");
+
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
+
+// // Requiring our routes
+// require("./routes/api-routes.js")(app);
+
+// // Syncing our database and logging a message to the user upon success
+// db.sequelize.sync().then(function () {
+//   app.listen(PORT, function () {
+//     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser." + PORT);
+//   });
+// });
