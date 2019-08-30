5e37d324e4d25fcab06dcb59f7f275ce960d4c28
cplank <cplankshepard@gmail.com>
2019-08-29 19:36:14 -0700

    changed dbuser

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -68,7 +68,7 @@ module.exports = function (app) {
   // grab data from image data ordered by most favorited
   app.get("/feed/orderbymostfavorited", function (req, res) {
     console.log("1. hello from liked users");
-    db.Image.findAll({
+    db.User.findAll({
       include: { model: db.Image, as: "likedUsers" },
     })
       .then(data => {
